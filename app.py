d0c556b1735951ee134a6ec618a9653293f072b2
gautamj-tech <55816462+gautamj-tech@users.noreply.github.com>
2020-10-17 22:04:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 import sqlite3
 import json
 from PIL import Image
-conn = sqlite3.connect('data.db')  #just for authentication
+conn = sqlite3.connect('data.db')
 c = conn.cursor()
 import hashlib
 def make_hashes(password):
@@ -32,7 +32,14 @@ def view_all_users():
 	c.execute('SELECT * FROM userstable')
 	data = c.fetchall()
 	return data
-
+def view_all_spam():
+	c.execute('SELECT * FROM spamtable')
+	data = c.fetchall()
+	return data
+def view_all_ham():
+	c.execute('SELECT * FROM hamtable')
+	data = c.fetchall()
+	return data
 def main():
     st.title("Built By Gautam Jain")
     html_temp = """
@@ -47,7 +54,7 @@ def main():
     choice = st.sidebar.selectbox("Menu", menu)
     if choice == "Home":
         st.subheader("Home")
-        st.info("This Is a Lyrics Generator Model Login to Generate Lyrics")
+        st.info("This Is a Lyrics Generation app. Please Login to access it")
         st.success("You Need To Sign Up to Access The Model")
     elif choice == "Login":
         st.subheader("Login Section")
@@ -64,13 +71,13 @@ def main():
                 artist = st.text_input("Singer Name")
                 song = st.text_input("Title")
                 st.warning('Click Here To Check Your Results')
-                if st.button("click here"):
-			url = "http://api.lyrics.ovh/v1/" + artist + '/' + song
-                        response = requests.get(url)
-                        json_data = json.loads(response.content)
-                        lyrics = json_data['lyrics']
-                        st.text("{}".format(lyrics))
-		        st.balloons()
+                if st.button("click here to get lyrics"):
+                    url = "http://api.lyrics.ovh/v1/" + artist + '/' + song
+                    response = requests.get(url)
+                    json_data = json.loads(response.content)
+                    lyrics = json_data['lyrics']
+                    st.text("{}".format(lyrics))
+                 
             else:
                 st.warning("Incorrect Username/Password")
     elif choice == "SignUp":
